2d1a50e587063e5f7d8d7fd99230b316bc40c799
Daniel Stenberg <daniel@haxx.se>
2020-07-02 23:26:25 +0200

    fixup the man page

--- docs/cmdline-opts/output-dir.d
+++ docs/cmdline-opts/output-dir.d
@@ -11,5 +11,8 @@ This option specifies the directory in which files should be stored, when
 The given output directory is used for all URLs and output options on the
 command line, up until the first --next.
 
+If the specified target directory doesn't exist, the operation will fail
+unless --create-dirs is also used.
+
 If this option is used multiple times, the last specified directory will be
 used.
